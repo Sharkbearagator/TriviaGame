4509277df4de44f5ed77d3d3a0f1297d975e8589
Tyler Nichols <tegulator98@gmail.com>
2019-06-09 23:06:22 -0600

    Psuedo code and added a place for timer in HTML

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+//Create a document ready state
+$(document).ready(function(){
+
+//create global variables
+var intervalId;
+
+//create a timer to start when the page loads
+
+//create a random question
+
+//fetch the answers for that specific question
+
+//create a way to select answers (buttons?)
+
+//alert if correct or not
+
+//wins and losses
+
+//reset clock and present new question
+});
